48082811a3e345de69757ad8822026e70bdfa2e5
Kahlil Oppenheimer <kahliloppenheimer@gmail.com>
2016-03-21 16:16:44 -0400

    Left aligned sidebar text to fix mobile spacing

--- public/js/about.js
+++ public/js/about.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+    shuffleList('#about-desc-list');
     $('.about-desc').textillate(
         {
             minDisplayTime: 1000,
@@ -7,6 +8,13 @@ $(document).ready(function() {
             in: {effect: 'fadeInDownBig', delay: 30},
             out: { delay: 30, effect: 'hinge', shuffle: true},
             loop: true,
-            selector: '.texts'
+            selector: '.texts',
         });
 });
+
+function shuffleList(listSelector) {
+    var ul = document.querySelector(listSelector);
+    for (var i = ul.children.length; i >= 0; i--) {
+        ul.appendChild(ul.children[Math.random() * i | 0]);
+    }
+}
